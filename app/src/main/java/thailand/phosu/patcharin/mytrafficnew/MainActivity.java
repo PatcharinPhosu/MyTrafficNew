a3e64302b171b227367dae5124420f167ab53ff2
patcharin <mam.embeeo@gmail.com>
2016-04-26 00:14:00 +0700

    try again

---
+++ app/src/main/java/thailand/phosu/patcharin/mytrafficnew/MainActivity.java
@@ -0,0 +1,23 @@
+package thailand.phosu.patcharin.mytrafficnew;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//Main Method
+
+    public void clickAboutMe(View view) {
+        Intent objIntent = new Intent(Intent.ACTION_VIEW);
+        objIntent.setData(Uri.parse("https://youtu.be/xLA-q_z14oA"));
+        startActivity(objIntent);
+
+    }//Click
+}
